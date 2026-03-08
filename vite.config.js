48f2a20f28022b7cbf2426cdb78669ff05a37b7c
yaejung-yoon <yaejung.yoon@aurorasunday.com>
2026-03-08 23:33:45 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8888',
+        target: 'http://localhost:3000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/.netlify/functions')
       }
     }
   }
